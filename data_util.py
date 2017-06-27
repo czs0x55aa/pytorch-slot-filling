1157aa90a6212ce26b81fee7796f6b535477b39e
czs0x55aa <czs0x55aa@gmail.com>
2017-06-27 11:43:35 +0800

    update

--- data_util.py
+++ data_util.py
@@ -8,7 +8,7 @@ from torch.autograd import Variable
 
 data_path = './data/atis.fold1.pkl.gz'
 
-def load_data(batch_size=1):
+def load_data():
     f = gzip.open(data_path, 'rb')
 
     train_set, valid_set, test_set, dicts = pickle.load(f)
@@ -18,24 +18,29 @@ def load_data(batch_size=1):
 
     train_x, _, train_label = train_set
     val_x, _, val_label = valid_set
+    test_x, _, test_label = test_set
 
     # Create index to word/label dicts
     w2idx, labels2idx = dicts['words2idx'], dicts['labels2idx']
     idx2w = {w2idx[k]: k for k in w2idx}
     idx2labels = {labels2idx[k]: k for k in labels2idx}
 
-    training = DataSet(train_x, train_label, idx2w, idx2labels, batch_size=batch_size)
-    return training
+    train_dataset = DataSet(train_x, train_label, idx2w, idx2labels)
+    val_dataset = DataSet(val_x, val_label, idx2w, idx2labels)
+    return train_dataset, val_dataset
 
 class DataSet(object):
     def __init__(self, src, target, idx2w, idx2labels, batch_size=1, shuffle=False):
         # assert np.shpae(data)[0] == np.shape(target)[0]
         self.data = zip(src, target)
-        if shuffle is True:
-            pass
+        # if shuffle is True:
+        #     pass
         # cannot support batch
         self.batch_size = batch_size
         self.length = np.shape(src)[0]
+
+        self.words = [list(map(lambda x: idx2w[x], w)) for w in src]
+        self.groundtruth = [list(map(lambda x: idx2labels[x], w)) for w in target]
 
         self.idx2w = idx2w
         self.idx2labels = idx2labels
@@ -60,4 +65,4 @@ class DataSet(object):
 
 
 if __name__ == '__main__':
-    load_data(1)
+    load_data()
--- main.py
+++ main.py
@@ -1,41 +1,47 @@
 # coding=utf8
+import torch
 import torch.nn as nn
 from torch import optim
+
 from data_util import load_data
 from model import SlotRNN
+from evaluate import conlleval
 
 embedding_size = 100
-batch_size = 10
-n_epochs = 30
+n_epochs = 10
 learning_rate = 0.01
 
-def eval():
-    pass
-
 def train():
-    dataset = load_data()
+    train_dataset, val_dataset = load_data()
 
-    model = SlotRNN(dataset.vocab_size, embedding_size, dataset.n_classes)
+    model = SlotRNN(train_dataset.vocab_size, embedding_size, train_dataset.n_classes)
     optimizer = optim.SGD(model.parameters(), lr=learning_rate)
     criterion = nn.NLLLoss()
     print (model)
 
+    def var2np(variable):
+        return torch.max(variable, 1)[1].data.squeeze(1).numpy()
+
     for epoch in range(n_epochs):
         # get batch data
         print_loss = 0
-        for data_x, data_y in dataset:
+        train_pred_label = []
+        for data_x, data_y in train_dataset:
             # zero_grad
             optimizer.zero_grad()
             #forward
-            output = model(data_x)
+            pred = model(data_x)
+            train_pred_label.append(var2np(pred))
             # compute loss
-            loss = criterion(output, data_y)
+            loss = criterion(pred, data_y)
             print_loss += loss.data[0]
             # backward
             loss.backward()
             optimizer.step()
-        print ('epoch: (%d / %d) loss: %.4f' % (epoch+1, n_epochs, print_loss/len(dataset)))
 
+        train_pred = [list(map(lambda x: train_dataset.idx2labels[x], y)) for y in train_pred_label]
+        print conlleval(train_pred, train_dataset.groundtruth, train_dataset.words, 'r.txt')
+        print ('epoch: (%d / %d) loss: %.4f' % (epoch+1, n_epochs, print_loss/len(train_dataset)))
 
 
 
